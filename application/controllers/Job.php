25346a1ef13ddd75faf1156991fe77e6cabcd1de
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2023-07-17 23:34:49 +0300

    crud job

---
+++ application/controllers/Job.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Job extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->database();
+        $this->load->helper('url');
+
+        $this->load->library('grocery_CRUD');
+    }
+
+    public function management()
+    {
+        try{
+            $crud = new grocery_CRUD();
+
+            $crud->set_table('job');
+            $crud->set_subject('Job');
+            $crud->set_rules('name', 'Name', 'required|alpha_numeric_spaces|max_length[100]', array('required' => 'Please set a name', 'alpha_numeric_spaces' => 'Please set a valid name.', 'max_length' => 'The name is too long.'));
+            $crud->unset_clone();
+            $crud->unset_read();
+            $output = $crud->render();
+
+            $this->load->view('grocery.php',(array)$output);
+
+        }catch(Exception $e){
+            show_error($e->getMessage().' --- '.$e->getTraceAsString());
+        }
+    }
+}
---
+++ application/views/grocery.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php 
+foreach($css_files as $file): ?>
+	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+<?php endforeach; ?>
+    <link rel="stylesheet" href="<?php echo site_url('assets/grocery_crud/css/bootstrap.min.css')?>">
+    <link rel="stylesheet" href="<?php echo site_url('assets/grocery_crud/css/bootstrap-grid.min.css')?>">
+    <link rel="stylesheet" href="<?php echo site_url('assets/grocery_crud/css/bootstrap-reboot.min.css')?>">
+
+</head>
+<body>
+	<div>
+		<a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> |
+		<a href='<?php echo site_url('examples/orders_management')?>'>Orders</a> |
+		<a href='<?php echo site_url('examples/products_management')?>'>Products</a> |
+		<a href='<?php echo site_url('examples/offices_management')?>'>Offices</a> | 
+		<a href='<?php echo site_url('examples/employees_management')?>'>Employees</a> |		 
+		<a href='<?php echo site_url('examples/film_management')?>'>Films</a> |
+		<a href='<?php echo site_url('examples/multigrids')?>'>Multigrid [BETA]</a>
+		
+	</div>
+    <?php echo date('Y-m-d H:i:s') ?>
+
+
+	<div style='height:20px;'></div>  
+    <div style="padding: 10px">
+		<?php echo $output; ?>
+    </div>
+    <?php foreach($js_files as $file): ?>
+        <script src="<?php echo $file; ?>"></script>
+    <?php endforeach; ?>
+
+
+</body>
+</html>
